c48ae9ad4e1add1c0a23f37a1754f55e3e6b4f76
ivanjimc <60380736+ivanjimc@users.noreply.github.com>
2020-02-21 12:46:34 -0800

    Update Program.cs

--- pelicula/Program.cs
+++ pelicula/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 
 namespace pelicula
-{
+{ class pelicula{
+        public string titulo;
+        public int16 año;
+        public string pais;
+        public string director;
+}
     class Program
     {
         static void Main(string[] args)
